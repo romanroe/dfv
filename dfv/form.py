d15f7bbe6468a6eae62f655da31514bc678a3e3f
Roman Roelofsen <romanroe@gmail.com>
2023-09-18 16:51:10 +0200

    - fixed create_form

--- dfv/form.py
+++ dfv/form.py
@@ -3,13 +3,14 @@ from typing import cast, TypeVar
 from django.forms import BaseForm
 from django.http import HttpRequest
 
-from dfv import is_get, is_post
+from dfv import is_get, is_head, is_post
+from dfv.view_stack import is_view_fn_request_target
 
 T_FORM = TypeVar("T_FORM", bound=BaseForm)
 
 
 def create_form(request: HttpRequest, form_class: type[T_FORM], **kwargs) -> T_FORM:
-    if is_get(request):
+    if is_get(request) or is_head(request) or not is_view_fn_request_target(request):
         form = form_class(**kwargs)
     else:
         form = form_class(data=request.POST, files=request.FILES, **kwargs)
